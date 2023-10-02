8c75c7deeb482d5bbba0f608e30ae93e785cbc17
Meghadri-Koley <meghadrikoley9@gmail.com>
2023-10-03 01:09:14 +0530

    Added product routes

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -3,21 +3,21 @@ const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router=express.Router();
 
 router.route("/projects")
-    .get() //users gets all the projects on public feed
-    .post() //mentor creates a new project
+    .get(isAuthenticatedUser,getAllProjects) //users gets all the projects on public feed
+    .post(isAuthenticatedUser,authorizeRoles("Mentor"),createProject) //mentor creates a new project
 
 router.route("/projects/:id")
-    .get() //users get project details (some details hidden based on type of user: public, mentor, mentee)
-    .post() //mentor edits his project details
+    .get(isAuthenticatedUser,getProjectDetails) //users get project details (some details hidden based on type of user: public, mentor, mentee)
+    .post(isAuthenticatedUser,authorizeRoles("Mentor"),editProject) //mentor edits his project details
 
 router.route("/projects/:id/apply")
-    .post() //users applying to a project
-    .delete() //users withdrawing their application
+    .post(isAuthenticatedUser,applyToProject) //users applying to a project
+    .delete(isAuthenticatedUser,withdrawApplication) //users withdrawing their application
 
 router.route("/projects/:id/mytasks")
-    .get() //selected mentees get their assigned tasks
+    .get(isAuthenticatedUser,getAssignedTasks) //selected mentees get their assigned tasks
 
 router.route("/projects/:id/tasks/:id")
-    .post() //selected mentees upload their work
+    .post(isAuthenticatedUser,uploadTaskWork) //selected mentees upload their work
 
 module.exports=router;
