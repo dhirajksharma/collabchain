5c26b8408f69b3d1b564b83ebfef04e09c529be5
Dhiraj Kumar Sharma <dhirajksharma.social@gmail.com>
2023-11-17 12:46:13 +0530

    modified blockchain contract

--- Prototype/blockchain/contracts/primaryContract.sol
+++ Prototype/blockchain/contracts/primaryContract.sol
@@ -2,93 +2,114 @@
 pragma solidity ^0.8.0;
 
 contract CollabChainTaskLog {
-    address public mentor;
+    //hash map for project ids corresponding to mentors and task ids
+    mapping(string => address) projectMentors;
+    mapping(bytes32 => Task) public tasks;
 
     //data structure to store task details
     struct Task {
+        string projectId;
         bool isComplete; //boolean to track task completion
-        string verificationKey; //verification key for mentor to mark the task complete
-        mapping(address => bool) assignedUsers; //array of users allocated to the task
-        mapping(bytes32 => Document) documents; //array of document ids and their content hash to verify against tampered documents
+        bytes32 verificationKey; //verification key for mentor to mark the task complete
+        address[] assignedUsers; //array of users allocated to the task
+        Document[] documents; //array of document ids and their content hash to verify against tampered documents
     }
 
     //data structure to store document details
     struct Document {
+        bytes32 documentId;
         string content;
     }
 
-    //hash map for tasks
-    mapping(bytes32 => Task) public tasks;
 
     //authroization checkers
-    modifier onlyMentor() {
-        require(msg.sender == mentor, "Only mentor can perform this action");
+    modifier onlyMentor(string memory projectId, address user) {
+        require(projectMentors[projectId] == user, "Only mentor can perform this action");
         _;
     }
 
-    modifier onlyAssigned(bytes32 taskId) {
-        require(tasks[taskId].assignedUsers[msg.sender], "You are not assigned to this task");
+    modifier onlyAssigned(bytes32 taskId, address user) {
+        bool isAssigned = false;
+        for (uint256 i = 0; i < tasks[taskId].assignedUsers.length; i++) {
+            if (tasks[taskId].assignedUsers[i] == user) {
+                isAssigned = true;
+                break;
+            }
+        }
+        require(isAssigned, "User is not assigned to this task");
         _;
     }
 
     //signals emitted by the blockchain upon a certain event
-    event TaskCreated(bytes32 taskId);
-    event UserAssigned(bytes32 taskId, address user);
-    event UserRemoved(bytes32 taskId, address user);
-    event DocumentCreated(bytes32 taskId, bytes32 documentId, string content, string verificationKey);
-    event TaskCompleted(bytes32 taskId);
-
-    //constructor stores identity of the mentor
-    constructor() {
-        mentor = msg.sender;
-    }
+    event ProjectCreated();
+    event TaskCreated();
+    event UserAssigned();
+    event UserRemoved();
+    event DocumentCreated(bytes32 verificationKey);
+    event TaskCompleted();
 
     //generate verification key
-    function generateKey() internal view returns (string memory) {
+    function generateKey() internal view returns (bytes32) {
         uint256 seed = uint256(block.timestamp) + uint256(blockhash(block.number - 1));
         return keccak256(abi.encodePacked(seed));
     }
 
+    //mentor creates a project
+    function createProject(string memory projectId) public{
+        projectMentors[projectId]=msg.sender;
+        emit ProjectCreated();
+    }
+
     //mentor creates a task
     //upon creation a verification key is generated which will be used later
-    function createTask(bytes32 taskId) public onlyMentor {
+    function createTask(string memory projectId, bytes32 taskId) public onlyMentor(projectId, msg.sender) {
         tasks[taskId] = Task({
+            projectId: projectId,
             isComplete: false,
-            verificationKey: generateKey()
+            verificationKey: generateKey(),
+            assignedUsers: new address[](0),
+            documents: new Document[](0)
         });
-        emit TaskCreated(taskId);
+        emit TaskCreated();
     }
 
     //mentor assigns a user to contribute to the task
-    function assignUser(bytes32 taskId, address user) public onlyMentor {
+    function assignUser(string memory projectId, bytes32 taskId, address user) public onlyMentor(projectId, msg.sender) {
         require(!tasks[taskId].isComplete, "Task is already complete");
-        tasks[taskId].assignedUsers[user] = true;
-        emit UserAssigned(taskId, user);
+        tasks[taskId].assignedUsers.push(user);
+        emit UserAssigned();
     }
 
     //mentor removes a user from the task
     //will add checks to verify from the user before removing
-    function removeUser(bytes32 taskId, address user) public onlyMentor {
+    function removeUser(string memory projectId, bytes32 taskId, address user) public onlyMentor(projectId, msg.sender) {
         require(!tasks[taskId].isComplete, "Task is already complete");
-        tasks[taskId].assignedUsers[user] = false;
-        emit UserRemoved(taskId, user);
+        address[] storage users = tasks[taskId].assignedUsers;
+        for (uint256 i = 0; i < users.length; i++) {
+            if (users[i] == user) {
+                users[i] = users[users.length - 1];
+                users.pop(); // Remove the last element
+                break;
+            }
+        }
+        emit UserRemoved();
     }
 
     //assigned users add documents to the project
     //when they upload the documents, they will be given the verification key
-    function createDocument(bytes32 taskId, bytes32 documentId, string memory content) public onlyAssigned(taskId) {
+    function createDocument(bytes32 taskId, bytes32 documentId, string memory content) public onlyAssigned(taskId, msg.sender) {
         require(!tasks[taskId].isComplete, "Task is already complete");
-        tasks[taskId].documents[documentId] = Document(content);
-        emit DocumentCreated(taskId, documentId, content, tasks[taskId].verificationKey);
+        tasks[taskId].documents.push(Document(documentId, content));
+        emit DocumentCreated(tasks[taskId].verificationKey);
     }
 
     //mentor marks the task complete by entering the verification key given to them by the user
     //verifying this key against our original key to check authenticity
-    function completeTask(bytes32 taskId, string key) public onlyMentor {
+    function completeTask(string memory projectId, bytes32 taskId, bytes32 key) public onlyMentor(projectId, msg.sender) {
         require(!tasks[taskId].isComplete, "Task is already complete");
-        require(keccak256(abi.encodePacked(key)) == keccak256(abi.encodePacked(verificationKey)), "Invalid verification key");
+        require(keccak256(abi.encodePacked(key)) == keccak256(abi.encodePacked(tasks[taskId].verificationKey)), "Invalid verification key");
         tasks[taskId].isComplete = true;
-        emit TaskCompleted(taskId);
+        emit TaskCompleted();
     }
 
     //central server uses this function to check task status
